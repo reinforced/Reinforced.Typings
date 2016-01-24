883e2a9873a662ee9ee3705603977e0908b09218
Pavel Novikov <pavel.b.novikov@gmail.com>
2016-01-24 17:16:48 +0600

    samples

--- Samples/Difficult/Reinforced.Typings.Samples.Difficult.CodeGenerators/Controllers/HomeController.cs
+++ Samples/Difficult/Reinforced.Typings.Samples.Difficult.CodeGenerators/Controllers/HomeController.cs
@@ -8,7 +8,12 @@ namespace Reinforced.Typings.Samples.Difficult.CodeGenerators.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult JQuery()
+        {
+            return View();
+        }
+
+        public ActionResult Angular()
         {
             return View();
         }
