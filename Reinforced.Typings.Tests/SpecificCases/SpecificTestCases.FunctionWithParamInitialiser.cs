38d7df6dd4170c9f8de02287b54d923da99b1b4b
Nikolaus Rosenmayr <nikolaus@smint.io>
2020-11-17 12:12:45 +0100

    fix: omit parameter initialiser in interfaces
    Methods in interface may not have parameter default values
    ("initialisers"). TypeScript compiler 3.9.7 emits error TS2371
    if such a default value is being found.
    
    Such default value makes a parameter optional. So, instead
    of emitting the default value, the parameter is marked with "?".
    Then in can be omitted from the list of parameters as it
    would be with a default value.
    
    fixes https://github.com/reinforced/Reinforced.Typings/issues/191

---
+++ Reinforced.Typings.Tests/SpecificCases/SpecificTestCases.FunctionWithParamInitialiser.cs
@@ -0,0 +1,73 @@
+﻿using System.Reflection;
+using Reinforced.Typings.Ast;
+using Reinforced.Typings.Attributes;
+using Reinforced.Typings.Fluent;
+using Reinforced.Typings.Generators;
+using Xunit;
+
+namespace Reinforced.Typings.Tests.SpecificCases
+{
+    public partial class SpecificTestCases
+    {
+        private class MyFunctionTestClassWithParamInitializer
+        {
+        
+            public string MyProperty { get; set; }
+        
+            public int MyNumber { get; set; }
+
+            public string DoSomething(int a = 4)
+            {
+                return a == 4 ? "Hello World" : string.Empty;
+            }
+        }
+
+        [Fact]
+        public void FunctionWithParamInitializerAsInterface()
+        {
+            const string result = @"
+                module Reinforced.Typings.Tests.SpecificCases {
+	                export interface IMyFunctionTestClassWithParamInitializer
+	                {
+		                MyProperty: string;
+		                MyNumber: number;
+		                doSomething(a?: number) : string;
+	                }
+                }
+            ";
+
+            AssertConfiguration(s =>
+            {
+                s.Global(x=>x.DontWriteWarningComment());
+                s.ExportAsInterface<MyFunctionTestClassWithParamInitializer>()
+                .WithPublicMethods(x=>x.CamelCase())
+                .WithPublicProperties();
+            }, result);
+        }
+
+        [Fact]
+        public void FunctionWithParamInitializerAsClass()
+        {
+            const string result = @"
+                module Reinforced.Typings.Tests.SpecificCases {
+	                export class MyFunctionTestClassWithParamInitializer
+	                {
+		                public MyProperty: string;
+		                public MyNumber: number;
+		                public doSomething(a: number = 4) : string
+		                {
+			                return null;
+		                }
+	                }
+                }
+                ";
+            AssertConfiguration(s =>
+            {
+                s.Global(x=>x.DontWriteWarningComment());
+                s.ExportAsClass<MyFunctionTestClassWithParamInitializer>()
+                    .WithPublicMethods(x=>x.CamelCase())
+                    .WithPublicProperties();
+            }, result);
+        }
+    }
+}
