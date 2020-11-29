2e4875102798ab25e9a69bb6595894a69bff3c4d
Pavel Novikov <pavel.b.novikov@gmail.com>
2020-11-29 08:26:18 +0300

    new project files + records unit test

---
+++ Reinforced.Typings.Tests/SpecificCases/SpecificTestCases.Records.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Reinforced.Typings.Fluent;
+using Xunit;
+
+namespace Reinforced.Typings.Tests.SpecificCases
+{
+    public record Person(string FirstName, string LastName);
+
+    public partial class SpecificTestCases
+    {
+        
+
+        [Fact]
+        public void Records()
+        {
+            const string result = @"
+module Reinforced.Typings.Tests.SpecificCases {
+	export interface IPerson
+	{
+		FirstName: string;
+		LastName: string;
+	}
+}";
+            AssertConfiguration(s =>
+            {
+                s.Global(a => a.DontWriteWarningComment());
+                s.ExportAsInterface<Person>().WithPublicProperties();
+            }, result);
+        }
+    }
+}
