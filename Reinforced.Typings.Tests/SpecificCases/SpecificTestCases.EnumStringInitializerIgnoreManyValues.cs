4218d6151dcc23107f9d92bc5be012ebef686179
Anton Pavlov <pavlov_as@skbkontur.ru>
2021-12-08 11:00:10 +0400

    исправил тест

--- Reinforced.Typings.Tests/SpecificCases/SpecificTestCases.EnumStringInitializerIgnoreManyValues.cs
+++ Reinforced.Typings.Tests/SpecificCases/SpecificTestCases.EnumStringInitializerIgnoreManyValues.cs
@@ -3,32 +3,32 @@ using Xunit;
 
 namespace Reinforced.Typings.Tests.SpecificCases
 {
-    public partial class SpecificTestCases
-    {
-        enum EnumToIgnoreSomeValues
-        {
-            Value1, 
-            Value2, 
-            Value3
-        }
+	public partial class SpecificTestCases
+	{
+		enum EnumToIgnoreSomeValues
+		{
+			Value1, 
+			Value2, 
+			Value3
+		}
         
-        [Fact]
-        public void EnumMultipleIgnoreValues()
-        {
-            const string result = @"
+		[Fact]
+		public void EnumMultipleIgnoreValuesShouldPass()
+		{
+			const string result = @"
 module Reinforced.Typings.Tests.SpecificCases {
-	export enum SomeInitializerEnum { 
+	export enum EnumToIgnoreSomeValues { 
         Value3 = ""Value3"" 
     }
 }";
-            AssertConfiguration(s =>
-            {
-                s.Global(a => a.DontWriteWarningComment());
-                s.ExportAsEnum<EnumToIgnoreSomeValues>()
-                    .Value(EnumToIgnoreSomeValues.Value1, d => d.Ignore())
-                    .Value(EnumToIgnoreSomeValues.Value2, d => d.Ignore())
-                    .UseString();
-            }, result);
-        }
-    }
+			AssertConfiguration(s =>
+			{
+				s.Global(a => a.DontWriteWarningComment());
+				s.ExportAsEnum<EnumToIgnoreSomeValues>()
+					.Value(EnumToIgnoreSomeValues.Value1, d => d.Ignore())
+					.Value(EnumToIgnoreSomeValues.Value2, d => d.Ignore())
+					.UseString();
+			}, result);
+		}
+	}
 }
